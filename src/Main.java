d116849f8f7cf98ea98486a232a0232a0839e297
Ilya <foreyk51@gmail.com>
2025-02-10 01:58:42 +0900

    Добавленны 4 и 5 задания.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //Задание 1
        double dog = 8;
        var cat = 3.6;
        var paper = 763789;
@@ -9,6 +10,7 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        //Задание 2
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
@@ -16,6 +18,7 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        //Задание 3
         dog = dog - 3.5;
         cat = cat - 1.6;
         paper = paper - 7639;
@@ -23,6 +26,26 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        //Задание 4
+        var friend = 19;
+        System.out.println("Задание 4");
+        System.out.println(friend);
+        friend = friend +2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        //Задание 5
+        var frog = 3.5;
+        System.out.println("Задание 5");
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+
 
     }
 }
