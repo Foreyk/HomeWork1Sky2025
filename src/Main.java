6a545dcbdf5539668f08491f50b922ecf9100054
Ilya <foreyk51@gmail.com>
2025-02-10 02:50:48 +0900

    Добавленны 6 и 7 задания.

--- src/Main.java
+++ src/Main.java
@@ -3,61 +3,63 @@
 public class Main {
     public static void main(String[] args) {
         //Задание 1
-//       double dog = 8;
-//       var cat = 3.6;
-//       var paper = 763789;
-//        System.out.println("Задание 1");
-//        System.out.println(dog);
-//        System.out.println(cat);
-//        System.out.println(paper);
+       double dog = 8;
+       var cat = 3.6;
+       var paper = 763789;
+        System.out.println("Задание 1");
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
 
-//        //Задание 2
-//        dog = dog + 4;
-//        cat = cat + 4;
-//        paper = paper + 4;
-//        System.out.println("Задание 2");
-//        System.out.println(dog);
-//        System.out.println(cat);
-//        System.out.println(paper);
+        //Задание 2
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println("Задание 2");
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
 
-//        //Задание 3
-//        dog = dog - 3.5;
-//        cat = cat - 1.6;
-//        paper = paper - 7639;
-//        System.out.println("Задание 3");
-//        System.out.println(dog);
-//        System.out.println(cat);
-//        System.out.println(paper);
+        //Задание 3
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println("Задание 3");
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
 
-//        //Задание 4
-//        var friend = 19;
-//        System.out.println("Задание 4");
-//        System.out.println(friend);
-//        friend = friend +2;
-//        System.out.println(friend);
-//        friend = friend / 7;
-//        System.out.println(friend);
-//
-//        //Задание 5
-//        var frog = 3.5;
-//        System.out.println("Задание 5");
-//        System.out.println(frog);
-//        frog = frog * 10;
-//        System.out.println(frog);
-//        frog = frog / 3.5;
-//        System.out.println(frog);
-//        frog = frog + 4;
-//        System.out.println(frog);
+        //Задание 4
+        var friend = 19;
+        System.out.println("Задание 4");
+        System.out.println(friend);
+        friend = friend +2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        //Задание 5
+        var frog = 3.5;
+        System.out.println("Задание 5");
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
 
         //Задание 6
-//        var weightBoxer1 = 78.2;
-//        var weightBoxer2 = 82.7;
-//        var totalWeightBoxers = weightBoxer1 + weightBoxer2;
-//        var diffWeightBoxers = totalWeightBoxers % weightBoxer1;
-//        System.out.println("Общий вес боксеров равен " + totalWeightBoxers + "кг! " + "Разница веса между боксерами
-//        состовляет " + diffWeightBoxers + "кг!" );
+     System.out.println("Задание 6");
+        var weightBoxer1 = 78.2;
+        var weightBoxer2 = 82.7;
+        var totalWeightBoxers = weightBoxer1 + weightBoxer2;
+        var diffWeightBoxers = totalWeightBoxers % weightBoxer1;
+        System.out.println("Общий вес боксеров равен " + totalWeightBoxers + "кг! "
+                + "Разница веса между боксерами состовляет " + diffWeightBoxers + "кг!" );
 
         //Задание 7
+     System.out.println("Задание 7");
         var totalJobHours = 640;
         var normalJobHours = 8;
         var totalWorkers = totalJobHours / normalJobHours;
