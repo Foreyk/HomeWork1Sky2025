773b0d41be890319ac8c6595dc5315bc7e88f8db
Ilya <foreyk51@gmail.com>
2025-02-10 01:36:52 +0900

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       double dog = 8;
+       var cat = 3.6;
+       var paper = 763789;
+        System.out.println("Задание 1");
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println("Задание 2");
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println("Задание 3");
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+    }
+}
+
